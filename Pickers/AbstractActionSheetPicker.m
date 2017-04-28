f554253dfe92590e4cc82550c615c1cb4483e906
Alexandre <alexandreudicf@gmail.com>
2017-04-28 11:23:57 -0300

    Change color background color

--- Pickers/AbstractActionSheetPicker.m
+++ Pickers/AbstractActionSheetPicker.m
@@ -237,6 +237,10 @@ CG_INLINE BOOL isIPhone4() {
 
 - (void)showActionSheetPicker {
     UIView *masterView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.viewSize.width, 260)];
+    
+    if (self.viewBackgroundColor) {
+        [masterView setBackgroundColor:self.viewBackgroundColor];
+    }
 
     // to fix bug, appeared only on iPhone 4 Device: https://github.com/skywinder/ActionSheetPicker-3.0/issues/5
     if (isIPhone4()) {
